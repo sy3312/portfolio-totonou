d7d7596a781a1dadaf13d55bca94dc2bfe64aae9
sy3312 <89714699+sy3312@users.noreply.github.com>
2022-02-23 17:16:09 +0900

    totonou commit
    新規webサイト　アップ

---
+++ js/script.js
@@ -0,0 +1,74 @@
+'use strict';
+// ハンバーガー
+const ham = document.getElementById('ham');
+const hamNav = document.getElementById('hamNav');
+
+ham.addEventListener('click', ()=> {
+    ham.classList.toggle('clickAction');
+    hamNav.classList.toggle('clickOpen');
+});
+
+// スライダー
+var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
+		if (windowwidth > 768){
+			var responsiveImage = [
+				{ src: '../img/slider/slider1.jpg'},
+				{ src: '../img/slider/slider2.jpg'},
+				{ src: '../img/slider/slider3.jpg'},
+				{ src: '../img/slider/slider4.jpg'}
+			];
+		} else {
+			var responsiveImage = [
+				{ src: '../img/slider/slider1.jpg'},
+				{ src: '../img/slider/slider2.jpg'},
+				{ src: '../img/slider/slider3.jpg'},
+				{ src: '../img/slider/slider4.jpg'}
+			];
+		}
+
+//Vegas全体の設定
+
+$('#slider').vegas({
+	overlay: true,
+	transition: 'blur',
+	transitionDuration: 2000,
+	delay: 10000,
+	animationDuration: 20000,
+	animation: 'kenburns',
+	slides: responsiveImage,
+});
+
+// header表示
+document.addEventListener('scroll', ()=> {
+	const scrollYEvent = window.pageYOffset;
+
+	if (0 < scrollYEvent) {
+		document.getElementById('js-headerContainer-scroll').classList.add('header-show');
+		document.getElementById('js-headerLeft').classList.add('headerToggle');
+		document.getElementById('js-headerRight').classList.add('headerToggle');
+	}else {
+		document.getElementById('js-headerContainer-scroll').classList.remove('header-show');
+		document.getElementById('js-headerLeft').classList.remove('headerToggle');
+		document.getElementById('js-headerRight').classList.remove('headerToggle');
+	}
+});
+
+// New Item
+$('.slider').slick({
+	autoplay: false,
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	prevArrow: '<div class="slick-prev"></div>',
+	nextArrow: '<div class="slick-next"></div>',
+	dots: true,
+	responsive: [
+		{
+		breakpoint: 769,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+		}
+	}
+]
+});
